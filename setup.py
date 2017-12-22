d639f2684e43f684d8761244c18350866dbfc299
stcoder <stcode98@foxmail.com>
2017-12-22 18:09:21 +0800

    基本功能

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(name='autopep8-dir',
+      version='0.0.1',
+      description='autopep8格式化整个文件夹的工具',
+      author='Stcoder',
+      author_email='stcode98@foxmail.com',
+      py_modules=['autopep8_dir'],
+      install_requires=['autopep8', 'docopt'],
+      entry_points={'console_scripts': ['autopep8_dir = autopep8_dir:main']},
+      zip_safe=False
+      )
